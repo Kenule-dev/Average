17cfc9fdc30be4a4751051ac5e0cab8dc492f8cf
Kenule-dev <139848788+Kenule-dev@users.noreply.github.com>
2026-02-08 19:46:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,5 @@ st.title('My machine learning app')
 
 st.info('My first ML app')
 
-url='https://raw.githubusercontent.com/Kenule-dev/Average/refs/heads/master/Exam_Score_Prediction.csv'
-df=pd.read_csv(url)
+df=pd.read_csv('https://raw.githubusercontent.com/Kenule-dev/Average/refs/heads/master/Exam_Score_Prediction.csv.csv')
 df
